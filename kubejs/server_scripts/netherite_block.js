23c1b8ec0baca10f28c6cc97725769aefbf97039
legoff-kaelig <legoff-kaelig@users.noreply.github.com>
2024-12-22 16:37:57 +0100

    Create netherite_block.js

---
+++ kubejs/server_scripts/netherite_block.js
@@ -0,0 +1,9 @@
+ServerEvents.recipes(event => {
+    event.shaped('minecraft:netherite_block', [
+      'AAA',
+      'AAA',
+      'AAA'
+    ], {
+      A: 'minecraft:netherite_ingot'
+    })
+  })
