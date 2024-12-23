31223c6c6430916053debecede32992efa081601
legoff-kaelig <legoff-kaelig@users.noreply.github.com>
2024-12-23 23:29:44 +0100

    Update remove_steel_ingot.js

--- kubejs/server_scripts/remove_steel_ingot.js
+++ kubejs/server_scripts/remove_steel_ingot.js
@@ -1,4 +1,10 @@
 ServerEvents.recipes(event => {
   event.remove({ type: 'minecraft:smelting', input: 'minecraft:iron_ingot', output: '#forge:ingots/steel' });
   event.remove({ type: 'minecraft:blasting', input: 'minecraft:iron_ingot', output: '#forge:ingots/steel' });
+
+  event.remove({ type: 'minecraft:smelting', input: 'minecraft:iron_nugget', output: '#forge:nuggets/steel' });
+  event.remove({ type: 'minecraft:blasting', input: 'minecraft:iron_nugget', output: '#forge:nuggets/steel' });
+
+  event.remove({ type: 'minecraft:smelting', input: 'minecraft:iron_block', output: '#forge:blocks/steel' });
+  event.remove({ type: 'minecraft:blasting', input: 'minecraft:iron_block', output: '#forge:blocks/steel' });
 });
